19222e5133d8c2b573327ec4ea56b6f3c4f315aa
Karolina Mizgier <karolinamizgier@gmail.com>
2021-10-06 20:04:29 +0200

    6.2 - added Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
      public static void main(String[] args) {
-          System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+          //Test dodawania
+          Calculator calculator = new Calculator();
+          Integer result = calculator.add(13,7);
+          Integer expectedResult = 20;
+          if(result.equals(expectedResult)) {
+               System.out.println("test OK");
+          }
+          else {
+               System.out.println("Error!");
+          }
+          // Test odejmowania
+          Calculator calculator2 = new Calculator();
+          Integer result2 = calculator2.subtract(13,7);
+          Integer expectedResult2 = 6;
+          if(result2.equals(expectedResult2)) {
+               System.out.println("test OK");
+          }
+          else {
+               System.out.println("Error!");
+          }
      }
 }

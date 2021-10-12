e84eabdd1cdce9c5246a6df7d018effa8e52df11
Karolina Mizgier <karolinamizgier@gmail.com>
2021-10-12 11:16:40 +0200

    6.3 - Collection code format and add given/when/then

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/collection/CollectionTestSuite.java
@@ -10,31 +10,39 @@ import java.util.List;
 
 public class CollectionTestSuite {
     @BeforeEach
-    public void before(){
+    public void before() {
         System.out.println("Test case: begin ");
     }
+
     @AfterEach
-    public void after(){
+    public void after() {
         System.out.println("Test case: end");
     }
 
     OddNumbersExterminator exterminator = new OddNumbersExterminator();
+
     @DisplayName("When the list is empty")
     @Test
-    public void testOddNumbersExterminatorEmptyList(){
+    public void testOddNumbersExterminatorEmptyList() {
+        //given
         List<Integer> numbers = new ArrayList<>();
+        //when
         List<Integer> result = exterminator.exterminate(numbers);
-        Assertions.assertEquals(numbers,result);
-        System.out.println("The list is empty. List size: "+result.size());
+        //then
+        Assertions.assertEquals(numbers, result);
+        System.out.println("The list is empty. List size: " + result.size());
     }
 
     @DisplayName("When the list contains odd and even numbers")
     @Test
-    public void testOddNumbersExterminatorNormalList(){
-    List<Integer> numbers = Arrays.asList(2, 13, 6, 27, 4, 30, 3);
-    List<Integer> result = exterminator.exterminate(numbers);
-    List<Integer> expectedResult = Arrays.asList(2,6,4,30);
-    Assertions.assertEquals(result,expectedResult);
-    System.out.println("List of odd numbers: "+result +"\n"+"Expected result: "+expectedResult);
+    public void testOddNumbersExterminatorNormalList() {
+        //given
+        List<Integer> numbers = Arrays.asList(2, 13, 6, 27, 4, 30, 3);
+        List<Integer> expectedResult = Arrays.asList(2, 6, 4, 30);
+        //when
+        List<Integer> result = exterminator.exterminate(numbers);
+        //then
+        Assertions.assertEquals(result, expectedResult);
+        System.out.println("List of odd numbers: " + result + "\n" + "Expected result: " + expectedResult);
     }
 }
